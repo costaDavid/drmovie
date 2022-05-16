221b22bfa15f0aca220dab8457c94a37c2b30418
David Costa <95049216+costaDavid@users.noreply.github.com>
2022-05-16 15:35:59 -0300

    Projeto iniciado

---
+++ src/main/java/com/DavidCosta/drmovie/DrmovieApplication.java
@@ -0,0 +1,13 @@
+package com.DavidCosta.drmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrmovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DavidCosta/drmovie/DrmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DavidCosta.drmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
